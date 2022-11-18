9322f7dd73e093bb02d92b42b8a494519a4a9248
vanchor <vovanyakorev2@gmail.com>
2022-11-18 16:35:07 +0100

    added baseRepositorium

---
+++ PhotoGallery/Repositories/Implementations/BaseRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using PhotoGallery.Data;
+using PhotoGallery.Repositories.Interfaces;
+
+namespace PhotoGallery.Repositories.Implementations
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly PhotoGalleryDbContext _context;
+
+        public BaseRepository(PhotoGalleryDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _context.Set<T>().AddRangeAsync(entities);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ PhotoGallery/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace PhotoGallery.Repositories.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void SaveChanges();
+
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task SaveChangesAsync();
+    }
+}
