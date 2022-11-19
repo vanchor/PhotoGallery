7746da8c36484290f15ac69bd10c1edf291aea23
dshym <dshim002@gmail.com>
2022-11-19 15:10:50 +0100

    added viewmodels helpers and components

---
+++ PhotoGallery/Models/BaseResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace PhotoGallery.Models
+{
+    public class BaseResponse<T>
+    {
+        public string Description { get; set; } = string.Empty;
+        public HttpStatusCode StatusCode { get; set; }
+        public T? Data { get; set; }
+    }
+}
